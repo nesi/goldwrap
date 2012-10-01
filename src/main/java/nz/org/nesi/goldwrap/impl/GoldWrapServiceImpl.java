28ab3b5601ed6bba6ca604d4f615fc820266aa26
Markus Binsteiner <m.binsteiner@auckland.ac.nz>
2012-10-01 14:21:28 +1300

    Minor

--- src/main/java/nz/org/nesi/goldwrap/impl/GoldWrapServiceImpl.java
+++ src/main/java/nz/org/nesi/goldwrap/impl/GoldWrapServiceImpl.java
@@ -11,6 +11,7 @@ import nz.org.nesi.goldwrap.domain.Organization;
 import nz.org.nesi.goldwrap.domain.Project;
 import nz.org.nesi.goldwrap.domain.User;
 import nz.org.nesi.goldwrap.errors.GoldCommandFault;
+import nz.org.nesi.goldwrap.util.GoldWrap;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
